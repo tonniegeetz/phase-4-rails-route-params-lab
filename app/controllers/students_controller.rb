9a380cb18aa6d21f3baf40883fd142b11394e7c8
tonniegeetz <tonniegeetz@gmail.com>
2024-07-24 10:20:18 +0300

    dynamic routing

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,11 @@
 class StudentsController < ApplicationController
-
   def index
     students = Student.all
     render json: students
   end
 
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
 end
